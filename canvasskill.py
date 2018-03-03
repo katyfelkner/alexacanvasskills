cf6aa889eb440616b0f431d32fba4312f53423f2
Katy Felkner <katy.felkner@ou.edu>
2018-03-03 15:11:34 -0600

    initial commit

---
+++ canvasskill.py
@@ -0,0 +1,24 @@
+# def canvas_skill(type=all, time=None, class=None)
+
+# parse the fucking file
+# calendar is a dict like structure?
+# want an iterable of events
+    # this is where we use the UIDs to find assignments
+
+
+
+
+
+# if type = homework
+
+
+
+# elif type = tests
+
+# else list all
+
+
+
+
+
+
